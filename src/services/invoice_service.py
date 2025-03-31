1a8fa0d6d9895f653dfdea3dd4759abf8aa0caa6
ajrlewis <hello@ajrlewis.com>
2025-03-31 14:58:19 +0100

    Refactored generate invoice method. Added bookings to add invoice method

--- src/services/invoice_service.py
+++ src/services/invoice_service.py
@@ -49,14 +49,7 @@ def get_invoice_by_id(invoice_id: int) -> Optional[Invoice]:
     return invoice
 
 
-def update_invoice_by_id(invoice_id: int, invoice_data: dict) -> Optional[Invoice]:
-    logger.debug(f"{invoice_id = } {invoice_data = }")
-    invoice = get_invoice_by_id(invoice_id)
-    logger.debug(f"{invoice = }")
-    if not invoice:
-        logger.error(f"Invoice {invoice_id} not found.")
-        return
-
+def update_invoice(invoice: Invoice, invoice_data: dict) -> Optional[Invoice]:
     if reference := invoice_data.get("reference"):
         logger.debug(f"{reference = }")
         invoice.reference = reference
@@ -118,6 +111,17 @@ def update_invoice_by_id(invoice_id: int, invoice_data: dict) -> Optional[Invoic
         return
 
 
+def update_invoice_by_id(invoice_id: int, invoice_data: dict) -> Optional[Invoice]:
+    logger.debug(f"{invoice_id = } {invoice_data = }")
+    invoice = get_invoice_by_id(invoice_id)
+    logger.debug(f"{invoice = }")
+    if not invoice:
+        logger.error(f"Invoice {invoice_id} not found.")
+        return
+    invoice = update_invoice(invoice, new_invoice_data)
+    return invoice
+
+
 def add_invoice(invoice_data: dict) -> Optional[Invoice]:
     new_invoice = Invoice(
         invoice_id=invoice_data.get("invoice_id"),
@@ -131,7 +135,7 @@ def add_invoice(invoice_data: dict) -> Optional[Invoice]:
         price_discount=invoice_data.get("price_discount"),
         price_total=invoice_data.get("price_total"),
         customer_id=invoice_data.get("customer_id"),
-        # bookings=invoice_data.get("bookings", []),
+        bookings=invoice_data.get("bookings", []),
         created_at=invoice_data.get("created_at"),
         created_by=invoice_data.get("created_by"),
         updated_at=invoice_data.get("updated_at"),
@@ -148,11 +152,9 @@ def add_invoice(invoice_data: dict) -> Optional[Invoice]:
         return
 
 
-def generate_invoice(invoice_data: dict) -> Optional[Invoice]:
-    customer_id = invoice_data["customer_id"]
-    date_start = invoice_data["date_start"]
-    date_end = invoice_data["date_end"]
-    created_by = invoice_data["created_by"]
+def generate_invoice_data(customer_id: int, date_start: str, date_end: str) -> dict:
+    reference_start = "W4LKIES"
+    days_due = 7
 
     # Get unique reference for invoice
     reference_hash = (
@@ -160,42 +162,74 @@ def generate_invoice(invoice_data: dict) -> Optional[Invoice]:
         .hexdigest()[:8]
         .upper()
     )
-    reference = f"W4LKIES-{reference_hash}"
+    reference = f"{reference_start}-{reference_hash}"
     logger.debug(f"{reference = }")
 
     # Get the customer bookings.
     bookings = booking_service.get_bookings(
         date_min=date_start, date_max=date_end, customer_id=customer_id
     )
+    logger.debug(f"Found {len(bookings)} bookings for invoice")
     logger.debug(f"{bookings = }")
 
     # Get the total price of the bookings
     price_subtotal = 0.0
+    price_discount = 0.0
     for booking in bookings:
         logger.debug(f"{booking.date} {booking.service.name} {booking.service.price}")
         price_subtotal += booking.service.price
-    price_discount = 0.0
     price_total = price_subtotal - price_discount
     logger.debug(f"{price_discount = } {price_total = }")
 
-    new_invoice_data = {
+    invoice_data = {
         "reference": reference,
         "date_start": date_start,
         "date_end": date_end,
         "date_issued": datetime.datetime.now(),
-        "date_due": datetime.datetime.now() + datetime.timedelta(days=7),
+        "date_due": datetime.datetime.now() + datetime.timedelta(days=days_due),
         "price_subtotal": price_subtotal,
         "price_discount": price_discount,
         "price_total": price_total,
         "customer_id": customer_id,
         "bookings": bookings,
-        "created_by": created_by,
     }
+
+    return invoice_data
+
+
+def generate_invoice(invoice_data: dict) -> Optional[Invoice]:
+    customer_id = invoice_data["customer_id"]
+    date_start = invoice_data["date_start"]
+    date_end = invoice_data["date_end"]
+
+    new_invoice_data = generate_invoice_data(customer_id, date_start, date_end)
+
+    created_by = invoice_data["created_by"]
+    new_invoice_data["created_by"] = created_by
+
     new_invoice = add_invoice(new_invoice_data)
     logger.info(f"{new_invoice = }")
     logger.info(f"{new_invoice.bookings = }")
 
     return new_invoice
+
+
+def regenerate_invoice_by_id(invoice_id: int, updated_by: int) -> Optional[Invoice]:
+    invoice = get_invoice_by_id(invoice_id)
+    if not invoice:
+        return
+
+    customer_id = invoice.customer_id
+    date_start = invoice.date_start
+    date_end = invoice.date_end
+
+    new_invoice_data = generate_invoice_data(customer_id, date_start, date_end)
+
+    new_invoice_data["updated_by"] = updated_by
+    new_invoice_data["updated_at"] = datetime.datetime.now()
+    # invoice = update_invoice_by_id(invoice_id, new_invoice_data)
+    invoice = update_invoice(invoice, new_invoice_data)
+    return invoice
 
 
 def download_invoice_by_id(invoice_id: int) -> Optional[bytes]:
