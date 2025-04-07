f4da9f2933e9259b0edd8e16539b832ed7054f9e
ajrlewis <hello@ajrlewis.com>
2025-04-07 11:39:59 +0100

    Fixed dumping and restoring of remote to locate database commands.

--- scripts/dump_and_restore.sh
+++ scripts/dump_and_restore.sh
@@ -2,12 +2,11 @@
 
 source .env
 
-backupfile="backups/w4lkies_db $(date +'%Y-%m-%d').bak"
-
+backupfile="backups/w4lkies_db $(date +'%Y-%m-%d').sql"
 echo $backupfile
 
 # Dump from remote
-pg_dump -Fc -v -d "postgresql://${remote_user}:${remote_password}@${remote_hostname}/${remote_dbname}?sslmode=require" -f "${backupfile}"
+pg_dump -F p -v --clean -d "postgresql://${remote_user}:${remote_password}@${remote_hostname}/${remote_dbname}?sslmode=require" -f "${backupfile}"
 
 # Restore to local
-pg_restore -v -c -d postgresql://${local_user}:${local_password}@${local_hostname}/${local_dbname} "${backupfile}"
+psql -v --clean -d "postgresql://${local_user}:${local_password}@${local_hostname}/${local_dbname}" -f "${backupfile}"
